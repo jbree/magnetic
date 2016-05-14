2843f51f7d313ec688a25feb1e2394b44370c824
Joshua Breeden <jbreeden@me.com>
2016-05-14 17:16:55 -0400

    add sendRequest function to make the request to the server. call sendRequest when the menu extension is selected.

--- Magnetic.safariextension/magnetic.js
+++ Magnetic.safariextension/magnetic.js
@@ -1,7 +1,43 @@
 safari.application.addEventListener('contextmenu', function (event) {
-  console.log(event);
   if(event.userInfo && event.userInfo.magnet) {
     event.contextMenu.appendContextMenuItem('sendMagnet', "Send Magnet to Transmission");
-    console.log(event.userInfo.magnet);
+
+    sendRequest({
+      url: event.userInfo.magnet,
+      server: safari.extension.settings.server,
+      id: '',
+      retry: 3
+    });
   }
 }, false);
+
+function sendRequest(config) {
+  var error = false;
+  var rpc = {
+    "method": "torrent-add",
+    "arguments": {
+      "filename": config.url
+    },
+  };
+  var req = new XMLHttpRequest();
+  //TODO cache this value
+  req.open('POST', config.server, true);
+  req.setRequestHeader('X-Transmission-Session-Id', config.id);
+  req.addEventListener('load', function () {
+    if(req.status == 409) {
+      console.log('Magnetic: received new session id');
+      config.id = req.getResponseHeader('X-Transmission-Session-Id');
+      if(--config.retry) {
+        console.log('Magnetic: resending request');
+        sendRequest(config);
+      } else {
+        console.log('Magnetic: giving up after 3 attempts');
+      }
+    }
+  });
+  req.addEventListener('error', function () {
+    console.log('Magnetic: error');
+  });
+
+  req.send(JSON.stringify(rpc));
+}
