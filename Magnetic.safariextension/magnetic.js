8b0c841a93f717391b7e19cce59d9d4a782229d3
Joshua Breeden <jbreeden@me.com>
2016-05-14 17:40:43 -0400

    save X-Transmission-Session-Id variable in user settings. This prevents us from getting a 409 response with every new request made,

--- Magnetic.safariextension/magnetic.js
+++ Magnetic.safariextension/magnetic.js
@@ -13,11 +13,20 @@ safari.application.addEventListener('command', function (event) {
     sendRequest({
       url: event.userInfo.magnet,
       server: safari.extension.settings.server,
-      id: '',
+      id: getSessionId(),
       retry: 3
     });
   }
 });
+
+// get the X-Transmission-Session-Id in user settings.
+function setSessionId(id) {
+  safari.extension.settings.sessionId = id;
+}
+// get the X-Transmission-Session-Id stored in the user settings.
+function getSessionId() {
+  return safari.extension.settings.sessionId;
+}
 
 function sendRequest(config) {
   var error = false;
@@ -28,21 +37,22 @@ function sendRequest(config) {
     },
   };
   var req = new XMLHttpRequest();
-  //TODO cache this value
   req.open('POST', config.server, true);
   req.setRequestHeader('X-Transmission-Session-Id', config.id);
   req.addEventListener('load', function () {
     if(req.status === 409) {
+      // 409 means we have the wrong session id. store it in settings and retry.
       console.log('Magnetic: received new session id');
-      config.id = req.getResponseHeader('X-Transmission-Session-Id');
+      setSessionId(req.getResponseHeader('X-Transmission-Session-Id'));
+      config.id = getSessionId();
+
       if(--config.retry) {
         console.log('Magnetic: resending request');
         sendRequest(config);
       } else {
         console.log('Magnetic: giving up after 3 attempts');
       }
-    }
-    if(req.status === 200) {
+    } else if(req.status === 200) {
       var res = JSON.parse(req.response);
       console.log('Magnetic: Server response \'' + res.result + '\'');
     }
