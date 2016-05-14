60a6edba4981343b7e6b60db89de19ef6ff581cc
Joshua Breeden <jbreeden@me.com>
2016-05-14 16:47:49 -0400

    add injection script to identify when magnets links are clicked and pass context to global script

---
+++ Magnetic.safariextension/inject.js
@@ -0,0 +1,29 @@
+document.addEventListener('contextmenu', function (event) {
+  var target = findAnchorNode(event.target);
+
+  if(target) {
+    // match strings beginning with `magnet:?` or `magnet://`
+    var url = target.href.match(/^(magnet:(?:[\?]|[\/\/]).*)/g);
+    if(url) {
+      // set context info for global extension to grab magnet url
+      safari.self.tab.setContextMenuEventUserInfo(event, {magnet:url[0]});
+      console.log(url);
+    }
+  }
+});
+
+/**
+ * given a DOM element, return the first 'A' anchor tag in its
+ * heirarchy, or null if one doesn't exist
+ *
+ * @param {DOMElement} element the element to start searching from
+ */
+function findAnchorNode (element) {
+  if(element.nodeName === 'A') {
+    return element;
+  } else if(element.parentElement.nodeName === 'BODY') {
+    return null;
+  } else {
+    return findAnchorNode(element.parentElement);
+  }
+}
