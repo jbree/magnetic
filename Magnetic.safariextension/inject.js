98c47d06dc48c4aaed048d81f310ccd7c4468615
Joshua Breeden <jbreeden@me.com>
2016-05-21 00:03:43 -0400

    inject HTML for popup panel when a magnet is clicked. this is still coming up improperly positioned

--- Magnetic.safariextension/inject.js
+++ Magnetic.safariextension/inject.js
@@ -1,14 +1,87 @@
 var magneticInject = (function () {
+  var isOpen = false;
+  var popup;
+  var magnetMatch = /^(magnet:(?:[\?]|[\/\/]).*)/g;
+  (function injectPopup() {
+    popup = document.createElement('div');
+    popup.className = 'magnetic';
+    popup.id = 'wrapper';
+    popup.innerHTML =
+    '<div id="notch"></div>' +
+    '<img src="' + safari.extension.baseURI + 'Icon-128.png">' +
+    '<span id="links">' +
+    '  <p><a href="" id="magnetic-plugin-enabled">Send to Server</a></p>' +
+    '  <p><a href="" id="magnetic-plugin-disabled">Download Locally</a></p>' +
+    '</span>';
+  }());
+  /**
+   * determine if an anchor with a magnet URL was clicked.
+   * if so, pass the magnet URL to the extension
+   */
   document.addEventListener('contextmenu', function (event) {
     var target = findAnchorNode(event.target);
 
     if(target) {
       // match strings beginning with `magnet:?` or `magnet://`
-      var url = target.href.match(/^(magnet:(?:[\?]|[\/\/]).*)/g);
+      var url = target.href.match(magnetMatch);
       if(url) {
         // set context info for global extension to grab magnet url
         safari.self.tab.setContextMenuEventUserInfo(event, {magnet:url[0]});
-        console.log(url);
+      }
+    }
+  });
+
+
+  /**
+   * hijack click events so we can:
+   * 1) open the popup when a magnet link is clicked
+   * 2) close the popup when outside the panel is clicked
+   * 3) handle popup link clicks
+   */
+  document.addEventListener('click', function (event) {
+    // close the popup if the user clicks outside of it
+    if(isOpen && !isRelative(popup, event.target)) {
+      popup.style.visibility = "hidden";
+      isOpen = false;
+    }
+
+    var target = findAnchorNode(event.target);
+    // if the click is on an anchor node
+    if(target) {
+      // match strings beginning with `magnet:?` or `magnet://`
+      var url = target.href.match(magnetMatch);
+
+      // if the url is a magnet
+      if(url) {
+        if(target.id === 'magnetic-plugin-disabled') {
+
+          console.log('download the magnet normally');
+        } else if (target.id === 'magnetic-plugin-enabled') {
+          // send this to the extension to handle
+          console.log('send magnet to the extension');
+          safari.self.tab.dispatchMessage('sendMagnet',{magnet:url[0]});
+          event.preventDefault();
+
+        } else {
+          // create the popup links
+          // popup = document.getElementById('wrapper');
+          target.parentElement.appendChild(popup);
+          wrapper.style.left = (event.clientX - 40 + document.body.scrollLeft + document.documentElement.scrollLeft) + 'px';
+          wrapper.style.top = (event.clientY + 24 + document.body.scrollTop + document.documentElement.scrollTop) + 'px';
+          wrapper.style.visibility = "visible";
+          isOpen = true;
+          console.log(event);
+          var nativeLink = document.getElementById('magnetic-plugin-disabled');
+          var serverLink = document.getElementById('magnetic-plugin-enabled');
+          nativeLink.href = url;
+          serverLink.href = url;
+
+          // hijack the click so the link isn't handled by
+          // local BT client
+          event.preventDefault();
+        }
+
+        // console.log(url);
       }
     }
   });
@@ -27,5 +100,24 @@ var magneticInject = (function () {
     } else {
       return findAnchorNode(element.parentElement);
     }
+  }
+
+  /**
+   * determine if one DOM element is an ancestor of another
+   *
+   * @param {DOMElement} ancestor
+   * @param {DOMElement} descendant
+   * @return true if ancestor is equal to or is an ancestor of descendant,
+   * false otherwise, and false if either parameters are falsy
+   */
+  function isRelative(ancestor, descendant) {
+    if(descendant && ancestor) {
+      if(ancestor === descendant) {
+        return true;
+      } else {
+        return isRelative(ancestor, descendant.parentElement);
+      }
+    }
+    return false;
   }
 }() );
